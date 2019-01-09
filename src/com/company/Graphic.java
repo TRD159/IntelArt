3159e965913fa7c7c014f0fb1afb56b6b7d9b662
Varun Kumar <varun.thegreate.kumar@gmail.com>
2019-01-09 08:55:59 -0600

    Added all necessary classes, add all methods to each of them...might fill those soon.

--- src/com/company/Graphic.java
+++ src/com/company/Graphic.java
@@ -7,6 +7,7 @@ class Graphic {
     Frame f = new Frame("Connect 5");
 }
 
+
 class Frame extends JFrame {
     Frame(String title) throws HeadlessException {
         super(title);
@@ -47,9 +48,98 @@ class Panel extends JPanel {
     }
 }
 
+abstract class Player{
+    private String name;
+    private char letter;
+
+    public Player(String name, char letter) {
+        this.name = name;
+        this.letter = letter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getLetter() {
+        return letter;
+    }
+
+    public abstract Move getMove(Board board);
+
+    public abstract Player freshCopy();
+
+    @Override
+    public String toString() {
+        return "("+name + " as " + letter + " )";
+    }
+}
+
+class HumanPlayer extends Player {
+    public HumanPlayer(String name, char letter) {
+        super(name, letter);
+    }
+
+    @Override
+    public Move getMove(Board board) {
+        return null;
+    }
+
+    @Override
+    public Player freshCopy() {
+        return null;
+    }
+}
+
+class RandomComputer extends Player {
+    public RandomComputer(String name, char letter) {
+        super(name, letter);
+    }
+    @Override
+    public Move getMove(Board board) {
+        return null;
+    }
+
+    @Override
+    public Player freshCopy() {
+        return null;
+    }
+}
+
+class Location {
+    int x, y , z;
+
+    public Location(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    @Override
+    public String toString() {
+        return "(" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                ")";
+    }
+}
+
 class Board {
     public static final char EMPTY = '-', RED = 'R', BLUE = 'B', PLAYING = '-', TIE = 'T';
-
+    public static final int X_SIZE=8,Y_SIZE=7, Z_SIZE = 8;
     private char[][][] board;
     private char winner;
 
@@ -72,6 +162,57 @@ class Board {
 
     public void setBoard(char[][][] board) {
         this.board = board;
+    }
+
+    public boolean makeMove(Move move, char p) {
+        return true;
+    }
+
+    public void setLocation(Location l, char p) {
+
+    }
+
+    public char getLocation() {
+        return ' ' ;
+    }
+
+    public char getWinner(){
+        return winner;
+    }
+
+    public boolean isFull(Move m) {
+        return false;
+    }
+
+    public void reset() {
+
+    }
+
+    public void draw() {
+
+    }
+}
+
+class Move {
+    private int row;
+    private int col;
+
+    public Move(int col, int row) {
+        this.col=col;
+        this.row=row;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + row + ",  " + col + ")";
     }
 }
 
